fc9472edd233c224c9ea03170b97e8b0d3cf641f
quitexitYusukeToyofuku <ec0962sr@gmail.com>
2022-08-23 16:17:25 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', 'PostController@index');
+
+
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/create', 'PostController@create');
+
+Route::post('/posts', 'PostController@store');
+
+Route::get('/posts/{post}', 'PostController@show');
+
